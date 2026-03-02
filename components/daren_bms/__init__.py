1ae3465f987ad7ca5d129ae6d125f710d1ead861
Sabin Iacob <iacobs+git@m0n5t3r.info>
2026-03-02 19:08:33 +0200

    no idea how this works

--- components/daren_bms/__init__.py
+++ components/daren_bms/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uart"]
 AUTO_LOAD = ["sensor", "binary_sensor"]
@@ -24,7 +25,7 @@ CONFIG_SCHEMA = DAREN_BMS_COMPONENT_SCHEMA.extend(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_DAREN_BMS_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
     cg.add_define("BUF_MAX_SIZE", 130)
