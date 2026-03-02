35f2ce15d3c132dc0bc602e9a9e89e091a7ddbef
Sabin Iacob <iacobs+git@m0n5t3r.info>
2026-03-02 19:41:40 +0200

    attempt tp pmake it see the sensors

--- components/daren_bms/sensor.py
+++ components/daren_bms/sensor.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 from esphome.components import sensor
 import esphome.config_validation as cv
 from esphome.const import (
+    CONF_ID,
     CONF_CURRENT,
     DEVICE_CLASS_BATTERY,
     DEVICE_CLASS_CURRENT,
